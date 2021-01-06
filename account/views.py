54015d6c32882a94ef324ffced975fb99df3edc7
kuseinova <sezimka.kuseinov@gmail.com>
2021-01-07 00:31:21 +0600

    register

--- account/views.py
+++ account/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import FormView, View
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .froms import UserRegistrationForm
+from .utils import send_activation_email
+
+
+class RegisterView(FormView):
+    """Registration user"""
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy('login')
+    template_name = 'account/registration.html'
+
+    def form_valid(self, form):
+        user = form.save()
+        send_activation_email(user)
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+
+
