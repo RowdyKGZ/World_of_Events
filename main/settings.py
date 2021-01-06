1cd703ae0d94cd80c916789e3ac1938411e8c960
kuseinova <sezimka.kuseinov@gmail.com>
2021-01-06 21:04:52 +0600

    first commit

--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^*mq!#pqe56dcb69u4_auhj-b$r-pcm3cyxt65y%tgl(047^u0'
+SECRET_KEY = config(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config(DEBUG)
 
 ALLOWED_HOSTS = []
 
