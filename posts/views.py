b79fcc9d0a86eaa2550972ceb0710c696f11d3d0
RowdyKGZ <RowdyKG@gmail.com>
2021-01-07 03:24:34 +0600

    сделал модельку таг и постов, подключил html для постов и статику подготовил

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+
+class PostListView(ListView):
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
+    template_name = 'posts/posts_list_view.html'
