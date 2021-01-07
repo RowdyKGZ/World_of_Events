b97b60e8a8964f5fe9901ce70c837f33ff43f226
RowdyKGZ <RowdyKG@gmail.com>
2021-01-07 23:49:20 +0600

    сделал crud

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
 
 class PostListView(ListView):
+    """Все посты"""
     queryset = Post.objects.all()
     context_object_name = 'posts'
-    template_name = 'posts/index.html'
+    template_name = 'posts/posts_list_view.html'
+
+
+class PostDetailView(DetailView):
+    """Детали постов"""
+    model = Post
+    context_object_name = 'post'
+    template_name = "posts/post_detail_view.html"
